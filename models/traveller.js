67fbd1494a68ec3da54bd2ba9d79a858601a737c
Christopher Croal <ccroal@ntlworld.com>
2018-12-19 16:26:53 +0000

    calculate total distance

--- models/traveller.js
+++ models/traveller.js
@@ -28,11 +28,13 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  return this.journeys
 };
 
 
